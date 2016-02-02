4817421d39fa979d5923ea9b7cbbe66141b18e81
agentronin316 <agentronin316@gmail.com>
2016-02-02 11:56:19 -0600

    Core Movement Engine framed out
    Data class for waypoints created.

---
+++ RailEngine/Assets/Scripts/ScriptEngine.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+/// <summary>
+/// @author Marshall Mason
+/// @description Core movement engine with data import and timeline tracking
+/// </summary>
+public class ScriptEngine : MonoBehaviour {
+
+    public List<ScriptWaypoint> waypoints;
+
+	void Start ()
+    {
+	    //Import Waypoint List
+        //StartCoroutine(MoveEngine);
+	}
+	
+    IEnumerator MoveEngine()
+    {
+        int currentWaypoint = 0;
+        while (currentWaypoint < waypoints.Count)
+        {
+            switch (waypoints[currentWaypoint].moveType)
+            {
+                case MoveType.WAIT:
+                    break;
+                case MoveType.STRAIGHT:
+                    //StartCoroutine(StraightMove);
+                    break;
+                case MoveType.BEZIER:
+                    //StartCoroutine(BezierMove);
+                    break;
+            }
+            yield return new WaitForSeconds(waypoints[currentWaypoint].moveTime);
+            currentWaypoint++;
+        }
+    }
+    
+}
---
+++ RailEngine/Assets/Scripts/ScriptWaypoint.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// @author Marshall Mason
+/// @description storage class for waypoint data
+/// </summary>
+public enum MoveType
+{
+    WAIT,
+    STRAIGHT,
+    BEZIER
+}
+public class ScriptWaypoint
+{
+    public MoveType moveType;
+    public float moveTime;
+    public Transform moveTarget;
+    public Transform curvePoint;
+}
