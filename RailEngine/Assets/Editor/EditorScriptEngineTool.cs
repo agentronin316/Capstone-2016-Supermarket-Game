5d856a7abb28be2262547089fe8e0551b10c0515
agentronin316 <agentronin316@gmail.com>
2016-03-04 10:35:42 -0600

    Started building pathing tool
    Editor window creation working, with error checking started.

---
+++ RailEngine/Assets/Editor/EditorScriptEngineTool.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class EditorScriptEngineTool : EditorWindow {
+
+    static ScriptEngine engine;
+
+    int curWaypoint;
+    int curFacing;
+
+
+    [MenuItem("Rail Tools/Open Rail Engine")]
+	public static void OpenEngineTool()
+    {
+        if (Selection.activeGameObject != null)
+        {
+            ScriptEngine engineScript = Selection.activeGameObject.GetComponent<ScriptEngine>();
+            if (engineScript != null)
+            {
+                engine = engineScript;
+                
+                EditorWindow.GetWindow(typeof(EditorScriptEngineTool), false, "Rail Pathing");
+            }
+            else
+            {
+                System.Media.SystemSounds.Exclamation.Play();
+                EditorUtility.DisplayDialog("No ScriptEngine detected on selected object.",
+                    "Please select a single game object with ScriptEngine and try again.", "Acknowledge");
+            }
+        }
+        else
+        {
+            System.Media.SystemSounds.Exclamation.Play();
+            EditorUtility.DisplayDialog("No game object selected.",
+                "Please select a single game object with ScriptEngine and try again.", "Acknowledge");
+        }
+    }
+
+    void OnGUI()
+    {
+        
+        if (curWaypoint >= engine.waypoints.Count || curWaypoint < 0)
+        {
+            if (engine.waypoints.Count == 0)
+            {
+                AddWaypoint();
+            }
+            curWaypoint = 0;
+        }
+
+        if (curFacing >= engine.facings.Count || curFacing < 0)
+        {
+            if (engine.facings.Count == 0)
+            {
+                AddFacing();
+            }
+            curFacing = 0;
+        }
+
+    }
+
+    void AddWaypoint()
+    {
+        ScriptWaypoint temp = new ScriptWaypoint();
+        temp.moveType = MoveType.WAIT;
+        temp.moveTime = 0f;
+        engine.waypoints.Add(temp);
+    }
+
+    void AddFacing()
+    {
+        ScriptFacings temp = new ScriptFacings();
+        temp.facingType = FacingType.FREE;
+        temp.facingTime = 0f;
+        engine.facings.Add(temp);
+    }
+}
