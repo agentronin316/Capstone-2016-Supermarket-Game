01b4a3090fdba3ea3a5ca160210bb861cd4d8ad7
Gus Catalano <augustin.catalano@gmail.com>
2016-04-26 11:30:15 -0500

    Made a thing
    Pause menu prefab

---
+++ RailEngine/Assets/Scenes/PauseButtons.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PauseButtons : MonoBehaviour {
+
+    public void quitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quit Button");
+    }
+    public void restartButton()
+    {
+        SceneManager.LoadScene("LevelDemo");
+        Debug.Log("Restart Button");
+    }
+}
